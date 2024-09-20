62b959e22c02ef9a53c4b3a1fef4ecc0948583db
Brody Broughton <broughton.brody07@gmail.com>
2024-09-19 22:06:25 -0600

    4.0 Basic Solve Final

--- jill.py
+++ jill.py
@@ -4,139 +4,92 @@ def main():
 
     ''' PARSING '''
 
-    # Description for parser
-    parser = argparse.ArgumentParser(description = 'open_files')
+    parser = argparse.ArgumentParser(description = 'open_files') # Description for parser
 
-    # Adding the dictionary and password to parser
-    parser.add_argument('passwords')
+    parser.add_argument('passwords') # Adding the dictionary and password to parser
     
     parser.add_argument('dictionary')
     
-    # Parsing args
-    args = parser.parse_args()
+    args = parser.parse_args() # Parsing args
 
 
 
     ''' PASSING CONTENTS OF PASSWORDS INTO ARRAY '''
     
-    # Opening the wordlist file
-    passwordsFile = open(args.passwords, 'r')
+    passwordsFile = open(args.passwords, 'r') # Opening the password file
     
-    # Array for all the words in wordlist
-    passwordsListArr = []
+    passwordsListArr = [] # Array for all the words in passwords
+    
+    for password in passwordsFile.readlines(): # For loop to iterate through every line in password
+        
+        passwordsListArr.append(password.strip()) # Adds each word to an array and strips newline characters
 
-    # For loop to iterate through every line in wordlist
-    for password in passwordsFile.readlines():
-
-        # Adds each word to an array and strips newline characters
-        passwordsListArr.append(password.strip())
-
-    # Close wordlist file
-    passwordsFile.close()
+    passwordsFile.close() # Close password file
 
 
 
     ''' PASSING CONTENTS OF WORDLIST INTO ARRAY '''
-
-    # Opening the wordlist file
-    wordListFile = open(args.dictionary, 'r')
     
-    # Array for all the words in wordlist
-    wordListArr = []
-
-    # For loop to iterate through every line in wordlist
-    for word in wordListFile.readlines():
-
-        # Adds each word to an array and strips newline characters
-        wordListArr.append(word.strip())
+    wordListFile = open(args.dictionary, 'r') # Opening the wordlist file
     
-    # Close wordlist file
-    wordListFile.close()
+    wordListArr = [] # Array for all the words in wordlist
+    
+    for word in wordListFile.readlines(): # For loop to iterate through every line in wordlist
+
+        wordListArr.append(word.strip()) # Adds each word to an array and strips newline characters
+    
+    wordListFile.close() # Close wordlist file
 
 
 
     ''' HASHING WORD LIST '''
+    
+    hashedWordListArr = [] # Array for the hashed passwords from wordlist
 
-    # Array for the hashed passwords from wordlist
-    hashedWordListArr = []   
+    for password in wordListArr: # For loop to go through each password in the wordlist array
 
-    # For loop to go through each password in the wordlist array
-    for password in wordListArr:
+        sha256_hash = hashlib.sha256() # Create a hash object
 
-        # Create a hash object
-        sha256_hash = hashlib.sha256()
-
-        # Converts the password string to byte password
-        currentBytePassword = password.encode()
-
-        # Update the hash object with bytes
-        sha256_hash.update(currentBytePassword)
-
-        # Get the hexadecimal representation of the hash
-        currentBytePassword = sha256_hash.hexdigest()
-
-        # Adds the current hashed password to the hashed word list array
-        hashedWordListArr.append(currentBytePassword)
+        currentBytePassword = password.encode() # Converts the password string to byte password
+        
+        sha256_hash.update(currentBytePassword) # Update the hash object with bytes
+        
+        currentBytePassword = sha256_hash.hexdigest() # Get the hexadecimal representation of the hash
+        
+        hashedWordListArr.append(currentBytePassword) # Adds the current hashed password to the hashed word list array
 
 
 
     ''' ITERATING THROUGH USER PASSWORDS AND SEPERATING THE USER HASHED PASSWORDS '''
 
-    # Final cracked passwords array
-    crackedPasswords = []
-
-    # Hashed user passwords array
-    hashedPasswords = []
-
-    # Temporary string for passing things into cracked array
-    tempStr = ''
-
-    # For loop that goes through the password array
-    for user in passwordsListArr:
-
-        # Another for loop that goes through the whole user string
-        for letter in user:
-            
-            # Adds each letter to the temporary string
-            tempStr += letter
-
-            # Remove the 'user:' from the string
-            user = user[:0] + user[1:]
+    crackedPasswords = [] # Final cracked passwords array
+    
+    hashedPasswords = [] # Hashed user passwords array
+    
+    tempStr = '' # Temporary string for passing things into cracked array
+    
+    for user in passwordsListArr: # For loop that goes through the password array
         
-            # Checks if the current letter is a colon to add the names of the users
-            if letter == ':':
-                
-                # Adds the temp string to the cracked passwords array
-                crackedPasswords.append(tempStr)
-
-                # Adds just the hashed password to the hashedPasswords array
-                hashedPasswords.append(user)
-
-                # Resets temp string
-                tempStr = ''
-
-                # Moves onto the next user
-                break
-    
-
-
-    ''' CHECKING IF A USER HASHED PASSWORD MATCHES WITH HASHED WORDLIST 
-    user = 0
-    
-    for userHash in hashedPasswords:
-
-        # For loop that goes through the hashed words in the hashed word list array
-        for hashWordList in hashedWordListArr:
+        for letter in user: # Another for loop that goes through the whole user string    
             
-            # Finally checks if the user password hash matches up with the hashed word
-            if userHash == hashWordList:
-                #print(user)
-                unhashedPassword = wordListArr[hashedWordListArr.index(userHash)]
+            tempStr += letter # Adds each letter to the temporary string
+            
+            user = user[:0] + user[1:] # Remove the 'user:' from the string
+            
+            if letter == ':': # Checks if the current letter is a colon to add the names of the users 
+                
+                crackedPasswords.append(tempStr) # Adds the temp string to the cracked passwords array
+                
+                hashedPasswords.append(user) # Adds just the hashed password to the hashedPasswords array
+                
+                tempStr = '' # Resets temp string
+                
+                break # Moves onto the next user
+    
 
-                crackedPasswords[user] += unhashedPassword
 
-                user += 1
-    '''
+    ''' CHECKING IF A USER HASHED PASSWORD MATCHES WITH HASHED WORDLIST '''
+    
     for userHash in hashedPasswords: # For loop that goes through the user hashed passwords
         
         if any(userHash == hashWordList for hashWordList in hashedWordListArr): # Checks if the user hash is in the hashed word list
@@ -145,10 +98,12 @@ def main():
 
         else:
 
-            crackedPasswords.pop(hashedPasswords.index(userHash))# += '' # If not found, adds nothing
-            
-    for x in crackedPasswords:
+            crackedPasswords.pop(hashedPasswords.index(userHash))# If a password isnt found, it removes that user and moves on
+
+
+    for x in crackedPasswords: # Finally prints out the users and their cracked passwords :)
         
         print(x)   
+
 if __name__ == "__main__":
     main()
